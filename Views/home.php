398716ba11565f1fc1f7349955133456bb785a70
LuvScientist <kingofcars3711@gmail.com>
2022-08-10 22:13:48 +0530

    changes in Navbar

--- Views/home.php
+++ Views/home.php
@@ -6,67 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnLine || Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="..\templates\assets\css\style.css">
   </head>
 <body>
-    <nav class="nav">
-        <a class="nav-link active"href="">Home</a>
-        <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Services</a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Orders</a></li>
-      <li><a class="dropdown-item" href="#">Wishlist</a></li>
-      <li><a class="dropdown-item" href="#">Settings</a></li>
-      <li><hr class="dropdown-divider"></li>
-      <li><a class="dropdown-item" href="#">Wallet</a></li>
-      <li><a class="dropdown-item" href="#">Sell your product</a></li>
-    </ul>
-  </li>
-            <div class="navbar-nav ms-auto text-center">
-                <button type="button" class="btn btn-light" id="Home-login">Login</button>
-                <button type="button" class="btn btn-outline-primary">Signup</button>
-            </div>    
-        <!-- <a class="nav-link"href="">Cart</a><i class="bi bi-5-square-fill"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-5-square-fill" viewBox="0 0 16 16">
-  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm5.994 12.158c-1.57 0-2.654-.902-2.719-2.115h1.237c.14.72.832 1.031 1.529 1.031.791 0 1.57-.597 1.57-1.681 0-.967-.732-1.57-1.582-1.57-.767 0-1.242.45-1.435.808H5.445L5.791 4h4.705v1.103H6.875l-.193 2.343h.064c.17-.258.715-.68 1.611-.68 1.383 0 2.561.944 2.561 2.585 0 1.687-1.184 2.806-2.924 2.806Z"/>
-</svg>
-        <a class="nav-link"href="">Profile</a> -->
-    </nav>
-    <div class="container-fluid">
-  <div class="row align-items-start">
-    <div class="container-sm">
-      <header><b>Electronics</b></header>
-      <img src="./../templates/assets/img/pexels-andrea-piacquadio-3769025.jpg" alt="No image">
-    </div>
-    <div class="col">
-        <header><b>Food & Supply</b></header>
-    </div>
-    <div class="col">
-      <header><b>Hardware</b></header>
-    </div>
-  </div>
-  <div class="row align-items-center">
-    <div class="col">
-     <header><b>Cosmatics</b></header>
-    </div>
-    <div class="col">
-      <header><b>Home & Furniture</b></header>
-    </div>
-    <div class="col">
-      <header><b>Music & Videos</b></header>
-    </div>
-  </div>
-  <div class="row align-items-end">
-    <div class="col">
-        <header><b>Sports</b></header>
-    </div>
-    <div class="col">
-      <header><b>Toys</b></header>
-    </div>
-    <div class="col">
-      <header><b>Others</b></header>
-    </div>
-  </div>
-</div>
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" id="navbar">
+          <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <!-- <a class="navbar-brand" href="#">Hidden brand</a> -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                    <ul class="navbar-nav">
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Services
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                          <li><a class="dropdown-item" href="#">Action</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                    
+                  <li class="nav-item">
+                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">About us</a>
+                  </li>
+                </ul>
+                <form class="d-flex col-6">
+                  <div class="col">
+                    <div class="input-group md-form form-sm form-2 pl-4">
+                      <input class="form-control mx-2 my-2 px-2 py-2 amber-border" type="text" placeholder="Search your product here..." aria-label="Search">
+                      <div class="col-6 col-md-4">
+                      <button type="button" class="btn btn-light col-4"><?php
+                          echo "<img src='../templates/assets/img/search.svg' >"; ?></button>
+                      </div>
+                      <button type="button" class="btn btn-dark col">Signup</button>
+                      <button type="button" class="btn btn-outline-light col">Login</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+          </div>
+        </nav>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
