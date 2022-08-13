b7615a2b15d2007b62140883650ca3d57a7d9d42
LuvScientist <kingofcars3711@gmail.com>
2022-08-13 13:14:48 +0530

    homepage update

--- Views/home.php
+++ Views/home.php
@@ -9,13 +9,12 @@
     <link rel="stylesheet" href="..\templates\assets\css\style.css">
   </head>
 <body>
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" id="navbar">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
           <div class="container-fluid">
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <!-- <a class="navbar-brand" href="#">Hidden brand</a> -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -42,7 +41,7 @@
                 <form class="d-flex col-6">
                   <div class="col">
                     <div class="input-group md-form form-sm form-2 pl-4">
-                      <input class="form-control mx-2 my-2 px-2 py-2 amber-border" type="text" placeholder="Search your product here..." aria-label="Search">
+                      <input class="form-control mx-2 my-2 px-2 py-0 amber-border" type="text" placeholder="Search your product here..." aria-label="Search">
                       <div class="col-6 col-md-4">
                       <button type="button" class="btn btn-light col-4"><?php
                           echo "<img src='../templates/assets/img/search.svg' >"; ?></button>
@@ -55,6 +54,13 @@
               </div>
           </div>
         </nav>
+        <div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col-md-auto p-2 mb-2 bg-dark text-white">
+      Electronics
+    </div>
+  </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
